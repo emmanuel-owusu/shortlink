fe6a6b2caa1bd8dc35f31098dd546175ea95f556
emmanuel-owusu <eman.kwame.owusu@gmail.com>
2024-05-13 00:03:29 -0400

    Add detailed comments to OpenApiConfig.java

--- src/main/java/com/github/emmanuelowusu/shortlink/OpenApiConfig.java
+++ src/main/java/com/github/emmanuelowusu/shortlink/OpenApiConfig.java
@@ -4,10 +4,21 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * The OpenApiConfig class provides configuration for OpenAPI documentation in the ShortLink application.
+ *
+ * @Configuration This annotation indicates that this class provides bean definitions for Spring configuration.
+ *
+ * @OpenAPIDefinition This annotation configures the generation of OpenAPI (Swagger) documentation for the application.
+ *   - info: Defines information about the API documented.
+ *       - title: The title of the API documentation.
+ *       - version: The version of the API documented.
+ *       - description: A brief description of the API functionalities.
+ */
 @Configuration
 @OpenAPIDefinition(
         info = @Info(
-                title = "ShortLink API",
+                title = "ShortLink ✨\uD83D\uDD17 API Documentation",
                 version = "1.0",
                 description = "Explore the available endpoints for ShortLink — a simple URL-shortening service"
         )
